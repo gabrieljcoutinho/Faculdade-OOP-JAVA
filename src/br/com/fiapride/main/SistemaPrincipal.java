62b4e2297d97efd63b2c064f4f55ffed904f868f
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-03-22 14:15:54 -0300

    atulização inicial antes de comecar

--- src/br/com/fiapride/main/SistemaPrincipal.java
+++ src/br/com/fiapride/main/SistemaPrincipal.java
@@ -1,22 +1,23 @@
 package br.com.fiapride.main;
+
 import br.com.fiapride.model.Carro;
+
 public class SistemaPrincipal {
-	public static void main(String[] args) {
-
-		// Exercicio do Violao
-		// Dentro do main...// Fabriquei a primeira (Instância 1)
-		Carro meuCarro = new Carro();
-		meuCarro.somEmDb = 10;
-		meuCarro.cor = "Preto";
-		meuCarro.material = "Aço";
-		// Fabriquei a segunda (Instância 2)
-		Carro carroDoJorge = new Carro();
-		carroDoJorge.somEmDb = 15;
-		carroDoJorge.cor = "Cinza";
-		carroDoJorge.material = "Aço";
-		System.out.println("Meu carro é: " + meuCarro.cor);
-		System.out.println("O do Jorge é: " + carroDoJorge.cor);
+ public SistemaPrincipal() {
 
 
-		}
-		}
+ }
+
+public static void main(String[] args) {
+ Carro meuCarro = new Carro();
+ meuCarro.somEmDb = 10;
+ meuCarro.cor = "Preto";
+ meuCarro.material = "Aço";
+ Carro carroDoJorge = new Carro();
+ carroDoJorge.somEmDb = 15;
+ carroDoJorge.cor = "Cinza";
+ carroDoJorge.material = "Aço";
+ System.out.println("Meu carro é: " + meuCarro.cor);
+ System.out.println("O do Jorge é: " + carroDoJorge.cor);
+}
+}
